c1f52d8576ae63e26c3c094d8ba13b9e71c3e681
ditoaugusta <dt_ap@ymail.com>
2019-03-04 12:08:53 +0700

    change orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:edukasi_mobile/app.dart';
 
-void main() => runApp(EdukasiApp());
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(EdukasiApp());
+}
