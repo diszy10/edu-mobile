997eae57015c7fde76e1618ced6bbad6bf99f2de
Angga Bachtiar <bachtiar.angga@gmail.com>
2019-03-06 15:54:01 +0700

    Commenting lines of test code

--- lib/widgets/notification.dart
+++ lib/widgets/notification.dart
@@ -25,28 +25,29 @@ class _EduNotificationState extends State<EduNotification> {
         onSelectNotification: onSelectNotification);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Plugin example app'),
-        ),
-        body: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              new RaisedButton(
-                onPressed: _showNotificationWithDefaultSound,
-                child: new Text('Show Notification With Default Sound'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  //Buat test notif
+  // @override
+  // Widget build(BuildContext context) {
+  //   return new MaterialApp(
+  //     home: new Scaffold(
+  //       appBar: new AppBar(
+  //         title: new Text('Plugin example app'),
+  //       ),
+  //       body: new Center(
+  //         child: new Column(
+  //           mainAxisAlignment: MainAxisAlignment.center,
+  //           mainAxisSize: MainAxisSize.max,
+  //           children: <Widget>[
+  //             new RaisedButton(
+  //               onPressed: _showNotificationWithDefaultSound,
+  //               child: new Text('Show Notification With Default Sound'),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
 
 // Buat cek informasi fungsi notifikasi
   Future onSelectNotification(String payload) async {
